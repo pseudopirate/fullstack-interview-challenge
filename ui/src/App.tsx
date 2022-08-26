4925a6de65776682a10d6b7011e6e2b4375a9abe
Igor Vernichenko <igorvern@yandex-team.ru>
2022-08-26 13:46:04 +0300

    feat: created ui template with create-react-app and material-ui

---
+++ ui/src/App.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@mui/material";
+
+
+// const darkTheme = createTheme({
+//     palette: {
+//         mode: 'dark',
+//     },
+// });
+
+const App = () => (
+    <div className="App">
+        <Button variant="contained">Button</Button>
+    </div>
+);
+
+export default App;
